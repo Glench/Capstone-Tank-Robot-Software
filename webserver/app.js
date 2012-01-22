101047a106abdb724463d0adfdb7eb9926a08e70
Glen Chiacchieri <www@glench.com>
2012-01-21 19:13:07 -0500

    Embed ip camera video in our web page.

--- webserver/app.js
+++ webserver/app.js
@@ -6,14 +6,36 @@
 var express = require('express')
   , routes = require('./routes')
   , io = require('socket.io')
+  , mustache = require('mustache')
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
+var tmpl = {
+    compile: function (source, options) {
+        if (typeof source == 'string') {
+            return function(options) {
+                options.locals = options.locals || {};
+                options.partials = options.partials || {};
+                if (options.body) // for express.js > v1.0
+                    locals.body = options.body;
+                return mustache.to_html(
+                    source, options.locals, options.partials);
+            };
+        } else {
+            return source;
+        }
+    },
+    render: function (template, options) {
+        template = this.compile(template, options);
+        return template(options);
+    }
+};
+
+
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -23,13 +45,15 @@ app.configure(function(){
   app.set("view options", {layout: false});
 
   // make a custom html template
-  app.register('.html', {
-    compile: function(str, options){
-      return function(locals){
-        return str;
-      };
-    }
-  });
+  // app.register('.html', {
+  //   compile: function(str, options){
+  //     return function(locals){
+  //       return str;
+  //     };
+  //   }
+  // });
+
+  app.register(".html", tmpl)
 
 });
 
--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -4,7 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index.html')
+  res.render('index.html',
+    {
+        locals: {
+            camera_ip: '192.168.1.2'
+        }
+    }
+  );
 };
 
 // websockets!
