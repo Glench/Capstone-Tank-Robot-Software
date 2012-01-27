c4a500ee70d62572492e432a743254cf41162e7c
Glen Chiacchieri <www@glench.com>
2012-01-26 19:04:18 -0500

    Organize motor controller code a little better.
    Getting ready for testing next week without encoders.

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -3,31 +3,71 @@
 // See http://arduino.cc/en/Tutorial/PWM for PWM information.
 // TODO: figure out a way to jump the serial queue for emergency stop
 
-int mosfetHighPin = 2; // always keep this one high
-int mosfetDisablePin = 4; // disables the h bridges, usually keep this low
-int mosfetForwardPin = 3;
-int mosfetBackwardPin = 5;
+// left motor
+int HighPin_left     = 2; // always keep this one high
+int DisablePin_left  = 4; // disables the h bridges, usually keep this low
+int ForwardPin_left  = 3;
+int BackwardPin_left = 5;
+
+// right motor
+int HighPin_right     = 2; // always keep this one high
+int DisablePin_right  = 4; // disables the h bridges, usually keep this low
+int ForwardPin_right  = 3;
+int BackwardPin_right = 5;
+
+
+// http://arduinoetcetera.blogspot.com/2011/01/classes-within-classes-initialiser.html
+// TODO: put this class in a .h file
 
 class Motor {
     int highPin_;
     int disablePin_;
     int forwardPin_;
     int backwardPin_;
+    // encode forward and backward as true and false
     boolean forward_ = true;
     boolean backward_ = false;
     private:
         void move(int, boolean);
+        int normalize_speed(int);
     public:
         void forward(int);
+        void backward(int);
 }
+
 Motor::Motor(int highPin, int disablePin, int forwardPin, int backwardPin) {
     highPin_ = highPin;
     disablePin_ = disablePin;
     forwardPin_ = forwardPin;
-    backwardPin_ = backwardPin_;
+    backwardPin_ = backwardPin;
 }
-void Motor::move(int speed, boolean direction) {
-    // TODO: add speed normalization
+
+void raise_exception(String message) {
+    // since there are no exceptions in Arduino-land, make my own fake exception
+    // to send back to the user
+    Serial.println(message);
+}
+
+int Motor:normalize_speed(int input) {
+    // for now, have 0, 1, 2, 3
+    // TODO: make this use division instead of constant
+    // probably (number of gradations - input) / 255
+    if (input == 0) {
+        return 0;
+    } elif (input == 1) {
+        return 85; // 255 / 3
+    } else if (input == 2) {
+        return 127; // 255 / 2
+    } else if (input == 3) {
+        return 255; // 255 / 1
+    } else {
+        raise_exception("Unrecognized input to motor" + input);
+    }
+
+}
+
+void Motor::move(int input, boolean direction) {
+    int speed = normalize_speed(input);
     if (direction_ == forward_) {
         analogWrite(highPin_, 255);
         analogWrite(forwardPin_, speed);
@@ -53,6 +93,30 @@ void setup()  {
     Serial.begin(9600);
 }
 
-void loop()  {
 
+Motor left_motor(HighPin_left, DisablePin_left, ForwardPin_left, BackwardPin_left);
+Motor right_motor(HighPin_right, DisablePin_right, ForwardPin_right, BackwardPin_right);
+
+int left_motor_input = 0;
+int right_motor_input = 0;
+
+void loop() {
+    // only read when available
+    if (Serial.available() > 0) {
+        left_motor_input = Serial.read();
+        right_motor_input = Serial.read();
+
+        // TODO: probably put this logic in Motor
+        if (left_motor_input > 0) {
+            left_motor.forward(left_motor_input);
+        } else if (left_motor_input < 0) {
+            left_motor.backward(left_motor_input);
+        }
+
+        if (right_motor_input > 0) {
+            right_motor.forward(right_motor_input);
+        } else if (right_motor_input < 0) {
+            right_motor.backward(right_motor_input);
+        }
+    }
 }
