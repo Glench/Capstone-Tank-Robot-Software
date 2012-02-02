1ded77b7749e5741c29a90a0a545e92d599ddbe1
Glen Chiacchieri <www@glench.com>
2012-02-02 18:05:46 -0500

    Move motor class into separate h file.
    I think this is necessary for arduino OO code to work.

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -3,6 +3,8 @@
 // See http://arduino.cc/en/Tutorial/PWM for PWM information.
 // TODO: figure out a way to jump the serial queue for emergency stop
 
+#include "motor.h"
+
 // left motor
 int HighPin_left     = 2; // always keep this one high
 int DisablePin_left  = 4; // disables the h bridges, usually keep this low
@@ -19,74 +21,10 @@ int BackwardPin_right = 10;
 // http://arduinoetcetera.blogspot.com/2011/01/classes-within-classes-initialiser.html
 // TODO: put this class in a .h file
 
-class Motor {
-    int highPin_;
-    int disablePin_;
-    int forwardPin_;
-    int backwardPin_;
-    // encode forward and backward as true and false
-    boolean forward_ = true;
-    boolean backward_ = false;
-    private:
-        void move(int, boolean);
-        int normalize_speed(int);
-    public:
-        void forward(int);
-        void backward(int);
-}
-
-Motor::Motor(int highPin, int disablePin, int forwardPin, int backwardPin) {
-    highPin_ = highPin;
-    disablePin_ = disablePin;
-    forwardPin_ = forwardPin;
-    backwardPin_ = backwardPin;
-}
-
 void raise_exception(String message) {
     // since there are no exceptions in Arduino-land, make my own fake exception
     // to send back to the user
     Serial.println(message);
-}
-
-int Motor:normalize_speed(int input) {
-    // for now, have 0, 1, 2, 3
-    // TODO: make this use division instead of constant
-    // probably (number of gradations - input) / 255
-    if (input == 0) {
-        return 0;
-    } elif (input == 1) {
-        return 85; // 255 / 3
-    } else if (input == 2) {
-        return 127; // 255 / 2
-    } else if (input == 3) {
-        return 255; // 255 / 1
-    } else {
-        raise_exception("Unrecognized input to motor" + input);
-    }
-
-}
-
-void Motor::move(int input, boolean direction) {
-    int speed = normalize_speed(input);
-    if (direction_ == forward_) {
-        analogWrite(highPin_, 255);
-        analogWrite(forwardPin_, speed);
-        analogWrite(backwardPin_, 0);
-        analogWrite(disable_, 0);
-    } else {
-        analogWrite(highPin_, 255);
-        analogWrite(forwardPin_, 0);
-        analogWrite(backwardPin_, speed);
-        analogWrite(disable_, 0);
-    }
-}
-
-// convenience methods
-void Motor::forward(int speed) {
-    move(forward_, speed);
-}
-void Motor::backward(int speed) {
-    move(backward_, speed);
 }
 
 void setup()  {
