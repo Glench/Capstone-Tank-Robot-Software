f8b54ec1848a8656d10b23a19ab370873624c967
Glen Chiacchieri <www@glench.com>
2012-03-01 13:23:13 -0500

    Start thinking about web interface sending data

--- webserver/public/javascripts/index.js
+++ webserver/public/javascripts/index.js
@@ -7,3 +7,43 @@ socket.on('connect', function() {
 socket.on('disconnect', function() {
     console.log('websocket disconnect')
 });
+
+// hash of arrow key code and if it's held down
+var keys = {
+    38: false, // up
+    40: false, // down
+    37: false, // left
+    39: false,  // right
+    should_send: false
+};
+
+$(document).keydown(function(evt) {
+    if (evt.which in keys && !keys[evt.which]) {
+        console.log('added');
+        keys[evt.which] = true;
+        keys['should_send'] = true;
+    }
+});
+
+$(document).keyup(function(evt) {
+    if (evt.which in keys && keys[evt.which]) {
+        console.log('removed');
+        keys[evt.which] = false;
+        keys['should_send'] = false;
+    }
+});
+
+
+// TODO: set timeout that sets timeout so this runs constantly polling for data
+// need mapping for speeds (get from slider) and also translation when holding
+// down forward+left (right needs to go faster) if possible.
+//
+// also need function for saying what things are held down so you can use mouse
+// as well
+//
+// also update gui to show 4 keys in correct arrow pattern
+var send_commands = function() {
+    if (keys['should_run']) {
+
+    }
+};
