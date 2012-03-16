e7101bf7a34cc46a469c1835c54ea5e504f2a772
Glen Chiacchieri <www@glench.com>
2012-03-15 20:18:08 -0400

    Mostly working web motor control code.
    Also have start of servo code.

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -4,6 +4,7 @@
 // TODO: figure out a way to jump the serial queue for emergency stop
 
 #include "motor.h"
+// #include "Servo.h"
 
 // left motor
 int HighPin_left     = 2; // always keep this one high, don't need this now
@@ -17,12 +18,14 @@ int DisablePin_right  = 12; // disables the h bridges, usually keep this low
 int ForwardPin_right  = 11;
 int BackwardPin_right = 6;
 
-
+// Servo servo;
+// in setup: servo.attach(pin) servo.write(angle from 0 to 180)
 void setup()  {
     Serial.begin(9600);
     // sometimes weird data on startup?
     Serial.flush();
 }
+
 
 
 Motor left_motor(HighPin_left, DisablePin_left, ForwardPin_left, BackwardPin_left);
--- webserver/public/javascripts/index.js
+++ webserver/public/javascripts/index.js
@@ -9,48 +9,100 @@ socket.on('disconnect', function() {
     // alert('Lost connection with robot!');
 });
 
+$( "#speed" ).slider({
+    value:4,
+    min: 4,
+    max: 6,
+    step: 1
+});
+
+
 // hash of arrow key code and if it's held down
-var keys = {
-    38: false, // up
-    40: false, // down
-    37: false, // left
-    39: false,  // right
+var key_map = {
+    38: 'up', // up
+    40: 'down', // down
+    37: 'left', // left
+    39: 'right',  // right
+};
+
+var inputs = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
     should_send: false
 };
 
+var translate_inputs_to_directions = function(speed, inputs) {
+    // speed is 4, 5, 6
+    var left_speed = 3,
+        right_speed = 3,
+        up = inputs.up,
+        down = inputs.down,
+        left = inputs.left,
+        right = inputs.right,
+        forward_backward_map = {6: 0, 5: 1, 4: 2},
+        encoded = {left: left_speed, right: right_speed};
+
+    if (up && !down) {
+        if (up && !(left || right)) {
+            encoded.left = encoded.right = speed;
+        } else if (left && !right) {
+            encoded.left = speed - 1;
+            encoded.right = speed;
+        } else if (!left && right) {
+            encoded.right = speed - 1;
+            encoded.left = speed;
+        }
+    } else if (!up && down) {
+        speed = forward_backward_map[speed];
+        if (down && !(left || right)) {
+            encoded.left = encoded.right = speed;
+        } else if (left && !right) {
+            encoded.left = speed + 1;
+            encoded.right = speed;
+        } else if (!left && right) {
+            encoded.right = speed + 1;
+            encoded.left = speed;
+        }
+    } else if (left && !right) {
+        encoded.left = forward_backward_map[speed];
+        encoded.right = speed;
+    } else if (!left && right) {
+        encoded.left = speed;
+        encoded.right = forward_backward_map[speed];
+    }
+    // success or failure case
+    return encoded;
+};
+
 $(document).keydown(function(evt) {
-    if (evt.which in keys && !keys[evt.which]) {
+    var direction = key_map[evt.which];
+    if (direction in inputs && !inputs[direction]) {
         evt.preventDefault();
-        keys[evt.which] = true;
-        keys['should_send'] = true;
+        inputs[direction] = true;
+        inputs['should_send'] = true;
     }
 });
 
 $(document).keyup(function(evt) {
-    if (evt.which in keys && keys[evt.which]) {
+    var direction = key_map[evt.which];
+    if (direction in inputs && inputs[direction]) {
         evt.preventDefault();
-        keys[evt.which] = false;
-        keys['should_send'] = false;
+        inputs[direction] = false;
+        inputs['should_send'] = false;
     }
 });
 
 // TODO: 
-// need mapping for speeds (get from slider) and also translation when holding
-// down forward+left (right needs to go faster) if possible.
-//
-// also need function for saying what things are held down so you can use mouse
-// as well
-//
-// also update gui to show 4 keys in correct arrow pattern
-var encoded_movement = {
-    left: 4,
-    right: 4
-};
+// fix left and right
+// stop from scrolling
+// fix forward to forward/left to forward
 
 var send_commands = function() {
-    if (keys['should_send']) {
+    if (inputs['should_send']) {
         // if the user wants it to go send instructions on websocket
-        socket.emit('move', encoded_movement)
+        socket.emit('move', translate_inputs_to_directions($('#speed').slider('value'), inputs))
     }
 };
 
