64f113e92fb9077d89ec69e3f5bfdc8e46b0efc2
Glen Chiacchieri <www@glench.com>
2012-03-29 18:11:15 -0400

    Make config for debugging easier.

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -3,26 +3,39 @@ var db = new sqlite.Database('db');
 var serialport = require('serialport')
 var SerialPort = serialport.SerialPort; // localize object constructor
 
-db.run('CREATE TABLE IF NOT EXISTS gps (id INTEGER PRIMARY KEY, latitude REAL, longitude REAL, speed REAL, is_repeater INTEGER)');
-// var gps_serial = new SerialPort('/dev/cu.usbserial-A40111OI', {
-//      parser: serialport.parsers.readline('\r'),
-//      baudrate: 4800
-// });
-// gps_serial.on('data', function(data){
-//     // $GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
-//     var gps_data_array = data.split(',');
-//     if (gps_data_array[0] == '$GPRMC' && gps_data_array[2] == 'A') {
-//         var gps_data = {
-//             lat: gps_data_array[3],
-//             lon: gps_data_array[5],
-//             speed: gps_data_array[7]
-//         }
-//         // TODO put these values in db
-//     }
-// });
-var motor_serial = new SerialPort("/dev/tty.usbserial-A600cJpP", {
-    parser: serialport.parsers.readline("\n")
-});
+var config = {
+    motor_serial: "/dev/tty.usbserial-A600cJpP",
+    gps_serial: '/dev/cu.usbserial-A40111OI',
+    camera_ip: '192.168.1.2',
+    motor_on: false,
+    gps_on: false
+};
+
+if (config.gps_on) {
+    db.run('CREATE TABLE IF NOT EXISTS gps (id INTEGER PRIMARY KEY, latitude REAL, longitude REAL, speed REAL, is_repeater INTEGER)');
+    var gps_serial = new SerialPort(config.gps_serial, {
+        parser: serialport.parsers.readline('\r'),
+        baudrate: 4800
+    });
+    gps_serial.on('data', function(data){
+        // $GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
+        var gps_data_array = data.split(',');
+        if (gps_data_array[0] == '$GPRMC' && gps_data_array[2] == 'A') {
+            var gps_data = {
+                lat: gps_data_array[3],
+                lon: gps_data_array[5],
+                speed: gps_data_array[7]
+            }
+            // TODO put these values in db
+        }
+    });
+}
+
+if (config.motor_on) {
+    var motor_serial = new SerialPort(config.motor_serial, {
+        parser: serialport.parsers.readline("\n")
+    });
+}
 
 /*
  * GET home page.
@@ -32,17 +45,19 @@ exports.index = function(req, res){
   res.render('index.html',
     {
         locals: {
-            camera_ip: '192.168.1.2'
+            camera_ip: config.camera_ip
         }
     }
   );
 };
 
-exports.get_gps = function(req, res) {
-    // db.each('SELECT * FROM gps;', function(err, rows) {
-    // send back on websocket
-    // });
-};
+if (config.gps_on) {
+    exports.get_gps = function(req, res) {
+        // db.each('SELECT * FROM gps;', function(err, rows) {
+        // send back on websocket
+        // });
+    };
+}
 
 // websockets!
 var io = require('socket.io')
@@ -55,6 +70,8 @@ exports.socket_connection = function(socket){
     socket.on('move', function (data) {
         console.log('movement command received', new Date(), data);
         // make sure to write string and not numberic values
-        motor_serial.write('' + data['left'] + data['right']);
+        if (config.motor_on) {
+            motor_serial.write('' + data['left'] + data['right']);
+        }
     });
 };
